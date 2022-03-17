ab4da269af98116c21bae7506227db7059cbf40d
Perry Fardella <perry.fardella@live.com>
2022-03-17 21:59:25 +0800

    Add TO DO

--- server/model/task.model.ts
+++ server/model/task.model.ts
@@ -1,21 +1,24 @@
-import { model, Schema, Model, Document } from 'mongoose';
+import { model, Schema, Model, Document } from "mongoose";
+
+// TO DO: Update these models once the data structures are defined for the journaling data
+// The author interface can probably stay
 
 export interface ITask extends Document {
-    name: string;
-    description: string;
-    createDate: string;
-    updatedDate: string;
-    createdBy: string;
-    updatedBy: string;
+  name: string;
+  description: string;
+  createDate: string;
+  updatedDate: string;
+  createdBy: string;
+  updatedBy: string;
 }
 
 const TaskSchema: Schema = new Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    createDate: { type: Date, default: Date.now },
-    updatedDate: { type: Date, default: Date.now },
-    createdBy: { type: String, required: false },
-    updatedBy: { type: String, required: false }
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  createDate: { type: Date, default: Date.now },
+  updatedDate: { type: Date, default: Date.now },
+  createdBy: { type: String, required: false },
+  updatedBy: { type: String, required: false },
 });
 
-export const TaskModel: Model<ITask> = model<ITask>('todos', TaskSchema);
+export const TaskModel: Model<ITask> = model<ITask>("todos", TaskSchema);
