6864dd6aa4ac62a82158487bfe138b09e1983c64
Perry Fardella <perry.fardella@live.com>
2022-03-20 16:40:29 +0800

    Update index.tsx

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,6 +4,7 @@ import RecordList from "../components/recordList";
 const Home: NextPage = () => {
   return (
     <div>
+      {/* need to pass date props to this component I think */}
       <RecordList />
     </div>
   );
