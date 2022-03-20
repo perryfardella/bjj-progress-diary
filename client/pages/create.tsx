9efc164ceea95f4938b7305b0be68c290afefa59
Perry Fardella <perry.fardella@live.com>
2022-03-20 10:29:07 +0800

    Turn the component function into an arrow function

--- client/pages/create.tsx
+++ client/pages/create.tsx
@@ -1,131 +1,96 @@
 import router from "next/router";
 import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 type Inputs = {
   sessionType: "gi" | "no gi";
   entry: string;
 };
 
-export default function Create() {
-  const [form, setForm] = useState({
-    name: "",
-    position: "",
-    level: "",
-  });
-
-  // These methods will update the state properties.
-  function updateForm(value: any) {
-    return setForm((prev) => {
-      return { ...prev, ...value };
-    });
-  }
-
-  // This function will handle the submission.
-  async function onSubmit(e: any) {
-    e.preventDefault();
-
-    // When a post request is sent to the create url, we'll add a new record to the database.
-    const newEntry = { ...form };
-
+const Create = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
     await fetch("http://localhost:3081/api/entry", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        entry: {
-          entry: "this is an entry",
-          user: "pez",
-        },
-      }),
-      // To do: replace placeholder hardcoded body with formData
-      // JSON.stringify(newEntry)
+      body: JSON.stringify({ ...data, user: "skrt" }),
     }).catch((error) => {
       window.alert(error);
       return;
     });
+  };
 
-    setForm({ name: "", position: "", level: "" });
-    router.push("/");
-  }
-
-  // This following section will display the form that takes the input from the user.
   return (
-    <div>
-      <h3>Create New Record</h3>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            value={form.name}
-            onChange={(e) => updateForm({ name: e.target.value })}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="position">Position</label>
-          <input
-            type="text"
-            className="form-control"
-            id="position"
-            value={form.position}
-            onChange={(e) => updateForm({ position: e.target.value })}
-          />
-        </div>
-        <div className="form-group">
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="positionOptions"
-              id="positionIntern"
-              value="Intern"
-              checked={form.level === "Intern"}
-              onChange={(e) => updateForm({ level: e.target.value })}
-            />
-            <label htmlFor="positionIntern" className="form-check-label">
-              Intern
-            </label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="positionOptions"
-              id="positionJunior"
-              value="Junior"
-              checked={form.level === "Junior"}
-              onChange={(e) => updateForm({ level: e.target.value })}
-            />
-            <label htmlFor="positionJunior" className="form-check-label">
-              Junior
-            </label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="positionOptions"
-              id="positionSenior"
-              value="Senior"
-              checked={form.level === "Senior"}
-              onChange={(e) => updateForm({ level: e.target.value })}
-            />
-            <label htmlFor="positionSenior" className="form-check-label">
-              Senior
-            </label>
-          </div>
-        </div>
-        <div className="form-group">
-          <input
-            type="submit"
-            value="Create person"
-            className="btn btn-primary"
-          />
-        </div>
-      </form>
-    </div>
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input type="radio" defaultValue="gi" {...register("sessionType")} />
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("entry", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.entry && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+
+    // <div>
+    //   <h3>Create New Record</h3>
+    //   <form onSubmit={onSubmit}>
+    //     <div className="form-group">
+    //       <label htmlFor="name">Name</label>
+    //       <input
+    //         type="text"
+    //         className="form-control"
+    //         id="name"
+    //         value={form.name}
+    //         onChange={(e) => updateForm({ name: e.target.value })}
+    //       />
+    //     </div>
+    //     <div className="form-group">
+    //       <label htmlFor="position">Position</label>
+    //       <input
+    //         type="text"
+    //         className="form-control"
+    //         id="position"
+    //         value={form.position}
+    //         onChange={(e) => updateForm({ position: e.target.value })}
+    //       />
+    //     </div>
+    //     <div className="form-group">
+    //       <div className="form-check form-check-inline">
+    //         <input
+    //           className="form-check-input"
+    //           type="radio"
+    //           name="positionOptions"
+    //           id="positionIntern"
+    //           value="Intern"
+    //           checked={form.level === "Intern"}
+    //           onChange={(e) => updateForm({ level: e.target.value })}
+    //         />
+    //         <label htmlFor="positionIntern" className="form-check-label">
+    //           Intern
+    //         </label>
+    //       </div>
+
+    // </div>
+    //     <div className="form-group">
+    //       <input
+    //         type="submit"
+    //         value="Create person"
+    //         className="btn btn-primary"
+    //       />
+    //     </div>
+    //   </form>
+    // </div>
   );
-}
+};
+
+export default Create;
