5f01541adc7be0ce09cfca5a3b109edd16ebc5b2
Perry Fardella <perry.fardella@live.com>
2022-03-22 21:39:49 +0800

    Begin dialog work for creating and editing entries

---
+++ client/components/create.tsx
@@ -0,0 +1,49 @@
+import router from "next/router";
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  sessionType: "gi" | "no gi";
+  entry: string;
+};
+
+const Create = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    await fetch("http://localhost:3081/api/entry", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ entry: { ...data, user: "skrt" } }),
+    }).catch((error) => {
+      window.alert(error);
+      return;
+    });
+    router.push("/");
+  };
+
+  return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <select {...register("sessionType")}>
+        <option value="gi">gi</option>
+        <option value="no gi">no gi</option>
+      </select>
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("entry", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.entry && <span>This field is required</span>}
+
+      <input type="submit" value="Submit entry" />
+    </form>
+  );
+};
+
+export default Create;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -5,25 +5,42 @@ import React from "react";
 import RecordList from "../components/recordList";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import isWeekend from "date-fns/isWeekend";
+import Dialog from "@mui/material/Dialog";
+import Create from "../components/create";
 
 const Home: NextPage = () => {
   const [value, setValue] = React.useState<Date | null>(new Date());
+  const [createDialogIsOpen, setcreateDialogIsOpen] =
+    React.useState<boolean>(false);
+  const [editDialogIsOpen, seteditDialogIsOpen] =
+    React.useState<boolean>(false);
 
   return (
     <div>
       <RecordList />
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <StaticDatePicker<Date>
-          orientation="landscape"
+          // orientation="landscape"
           openTo="day"
           value={value}
-          shouldDisableDate={isWeekend}
+          // shouldDisableDate={isWeekend}
           onChange={(newValue) => {
             setValue(newValue);
           }}
           renderInput={(params) => <TextField {...params} />}
         />
       </LocalizationProvider>
+      <Dialog
+        onClose={() => setcreateDialogIsOpen(false)}
+        open={createDialogIsOpen}
+      >
+        {/* On submission need to close the dialog, can pass setDialogIsOpen(false) down as a function prop that's called on submission */}
+        <Create />
+      </Dialog>
+
+      <button onClick={() => setcreateDialogIsOpen(true)}>
+        create new record
+      </button>
     </div>
   );
 };
