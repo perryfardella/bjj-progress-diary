9b8c5c6521132a9b883fdb233675a0ba9d9125cb
Perry Fardella <perry.fardella@live.com>
2022-03-26 09:41:19 +0800

    Work on mui/next integration

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,15 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
+import theme from "../src/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
